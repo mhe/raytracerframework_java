af2c130b48d1b061417949b01f0fec6005c40794
Maarten Everts <maarten@bitpuzzle.com>
2009-01-12 21:04:43 +0100

    Add support for writing image to PNG file.
    Note: untested!

--- src/raytracer/Image.java
+++ src/raytracer/Image.java
@@ -5,6 +5,10 @@
 
 package raytracer;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.vecmath.Color3f;
 
 /**
@@ -53,8 +57,24 @@ public class Image {
         return y*width + x;
     }
 
+    protected int[] toIntArray() {
+        int [] intArray = new int[width*height];
+        for(int i=0;i < width*height;++i) {
+            Color3f c = colors[i];
+            intArray[i] = (int)(c.x * 255)*256*256+(int)(c.y*255)*256+(int)(c.z*255);
+        }
+        return intArray;
+    }
     public void writeToPNG(String filename) {
         // TODO: to implement
+        BufferedImage bImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+        bImage.setRGB(0, 0, width, height, toIntArray(), 0, width);
+        File f = new File(filename);
+        try {
+            ImageIO.write(bImage, "png", f);
+        } catch (IOException e) {
+            System.err.println("Exception: unable to write image to file "+filename);
+        }
     }
 
 }
